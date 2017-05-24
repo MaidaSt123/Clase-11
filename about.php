5016e43dca37f44137cae7ea5c604a9f4d505111
MaidaSt123 <mmstephens@uc.cl>
2017-05-24 13:08:27 -0400

    Antecedentes y referentes para mi portafolio

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php include('header.php')?>
+
+<div class="row">
+<div class="col-sm-12">
+<h3>Mis trabajos</h3>
+<p>Esta página la quiero utilizar para hacer mi portafokio de diseño. A través de esta página quiero mostrar los proyectos que he realizado a lo largo de toda mi carrera y otros trabajos realizados por cuenta propia. Quiero utilizar esta plataforma como medio para que los clientes vean los diferentes proyectos que he hecho y sus caracteristicas, para que conozcan sobre mi, mis capacidades y conocimientos. Los referentes antes mostrados son una idea de como quiero que se vea el portafolio, busco una página simple, con colores suaves, que den al cliente la sensación de proyectos bien construidos y limpios, poniendo en valor los objetos mostrados a través de la fotografía.</p>
+</div><!--/col-sm-12-->
+</div><!--/row->
+
+<?php include('footer.php')?>
